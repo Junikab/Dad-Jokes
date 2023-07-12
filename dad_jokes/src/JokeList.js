25d4121059807f33df3fa899ebbfa0e26930acba
Jenny <junikab@gmail.com>
2023-07-12 11:28:37 +1000

    141. sorting jokes

--- dad_jokes/src/JokeList.js
+++ dad_jokes/src/JokeList.js
@@ -86,6 +86,7 @@ class JokeList extends Component {
                 </div>
             );
         }
+        let jokes = this.state.jokes.sort((a, b)=> b.votes - a.votes)
         return (
             <div className="JokeList">
                 <div className="JokeList-sidebar">
@@ -97,7 +98,7 @@ class JokeList extends Component {
                     <button className="JokeList-getMore" onClick={this.handleClick}>New Jokes</button>
                 </div>
                 <div className="JokeList-jokes">
-                    {this.state.jokes.map((j) => (
+                    {jokes.map((j) => (
                         <Joke
                             key={j.id}
                             votes={j.votes}
